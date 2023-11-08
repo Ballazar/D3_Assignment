873d7890ecefceb732c8164939d1f421f64d198b
ErykGlica <erykusn@gmail.com>
2023-11-08 15:56:34 +0000

    d

--- script/d3script.js
+++ script/d3script.js
@@ -16,7 +16,7 @@
 
     const pathGenerator = d3.geoPath().projection(projection)
 // loading the JSON data and passing it through the path to create the map
-        d3.json("json/uk.json").then(data => 
+        d3.json("C:/Users/eryku/OneDrive/Documents/GitHub/D3_Assignment/Geojson").then(data => 
         {
             svg.selectAll("path")
             .data(data.features)
