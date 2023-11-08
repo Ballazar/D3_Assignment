f5addeac701b8489d0d96c9bdb6e3610e6d60bb1
ErykGlica <erykusn@gmail.com>
2023-11-08 16:11:51 +0000

    Update d3script.js

--- script/d3script.js
+++ script/d3script.js
@@ -16,7 +16,7 @@
 
     const pathGenerator = d3.geoPath().projection(projection)
 // loading the JSON data and passing it through the path to create the map
-        d3.json("Geojson/uk.json").then(data => 
+        d3.json("https://github.com/Ballazar/D3_Assignment/blob/main/Geojson/uk.json").then(data => 
         {
             svg.selectAll("path")
             .data(data.features)
